a83a109e41f223b7c9a4c86d77f1c2c15503d6a6
KRKimDongWoo <dhsodhao52@naver.com>
2020-01-09 14:11:51 +0900

    update source code

--- src/component/menu/Footer.js
+++ src/component/menu/Footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './Footer.css'
 
+const githubLink = "https://github.com/KRKimDongWoo/alcoholic"
 class Footer extends Component {
 	render() {
 		return(
@@ -12,7 +13,12 @@ class Footer extends Component {
 					본 웹페이지는 컴퓨터공학부 김동우가 술 마시려고 만든 웹페이지로, 언론정보학과 조예은 양과 화학생물공학부 이기범 군의 재촉으로 완성되었습니다.
 				</p>
 				<p className='footer-content'>
-					source code not available yet.
+					{'source code now available on '}
+					<a 
+						href={githubLink} 
+						style={{color: 'blue'}}>
+						here
+					</a>.
 				</p>
 			</div>
 		)
